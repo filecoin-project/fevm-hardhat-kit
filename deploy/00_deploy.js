a9cfb9de41a2bedf0c81ab6d6e76d87160c57dbd
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-10-18 06:05:12 +0200

    catch errors

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -44,30 +44,42 @@ module.exports = async ({ deployments }) => {
   const priorityFee = await callRpc("eth_maxPriorityFeePerGas");
   const nonce = await callRpc("Filecoin.MpoolGetNonce", [f1addr]);
   console.log('nonce:', nonce);
-  // console.log("Ethereum deployer address:", deployer.address);
+  console.log("Ethereum deployer address:", deployer.address);
   console.log("Send faucet funds to this address (f1):", f1addr);
   // If the address has not recieved Filecoin yet, this line will fail. Go to faucet.
   let actorId = await callRpc('Filecoin.StateLookupID', [f1addr, []]);
   actorIdDecimal = Number(actorId.slice(1)).toString(10);
   actorId = Number(actorId.slice(1)).toString(16);
-  const f0addr = '0xff' + '0'.repeat(38-actorId.length) + actorId;
+  const f0addr = '0xff' + '0'.repeat(38 - actorId.length) + actorId;
 
   // console.log('Filecoin deployer address f0', "f0" + actorIdDecimal)
   console.log('Ethereum deployer address (from f0):', f0addr);
   console.log("priorityFee: ", priorityFee);
-
-  // await deploy("SimpleCoin", {
-  await deploy("SimpleCoin", {
-    from: deployer.address,
-    args: [],
-    // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
-    // a large gasLimit. This should be addressed in the following releases.
-    gasLimit: 1000000000, // BlockGasLimit / 10
-    // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
-    // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-    maxPriorityFeePerGas: priorityFee,
-    nonce: nonce,
-    log: true,
-  });
+  console.log("--")
+  try {
+    await deploy("SimpleCoin", {
+      from: deployer.address,
+      args: [],
+      // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+      // a large gasLimit. This should be addressed in the following releases.
+      gasLimit: 1000000000, // BlockGasLimit / 10
+      // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
+      // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
+      maxPriorityFeePerGas: priorityFee,
+      nonce: nonce,
+      log: true,
+    });
+  } catch (e) {
+    console.log("--")
+    console.log("Deployment data:", {
+      from: deployer.address,
+      args: [],
+      gasLimit: 1000000000,
+      maxPriorityFeePerGas: priorityFee,
+      nonce: nonce,
+      log: true,
+    })
+    console.log("Error:", e.message)
+  }
 };
 module.exports.tags = ["SimpleCoin"];
