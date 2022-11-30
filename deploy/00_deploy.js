971173c33d909a65d5d68e67852e831bc852940a
ZPrayag <prayag.a@zebpay.com>
2022-11-30 16:08:48 +0530

    Updated SimpleCoin arguments

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -1,85 +1,85 @@
-require("hardhat-deploy");
-require("hardhat-deploy-ethers");
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
 
-const ethers = require("ethers");
-const fa = require("@glif/filecoin-address");
-const util = require("util");
-const request = util.promisify(require("request"));
+const ethers = require("ethers")
+const fa = require("@glif/filecoin-address")
+const util = require("util")
+const request = util.promisify(require("request"))
+const { networkConfig } = require("../helper-hardhat-config")
 
-const DEPLOYER_PRIVATE_KEY = network.config.accounts[0];
+const DEPLOYER_PRIVATE_KEY = network.config.accounts[0]
 
 function hexToBytes(hex) {
-  for (var bytes = [], c = 0; c < hex.length; c += 2)
-    bytes.push(parseInt(hex.substr(c, 2), 16));
-  return new Uint8Array(bytes);
+    for (var bytes = [], c = 0; c < hex.length; c += 2) bytes.push(parseInt(hex.substr(c, 2), 16))
+    return new Uint8Array(bytes)
 }
 
 async function callRpc(method, params) {
-  var options = {
-    method: "POST",
-    url: "https://wallaby.node.glif.io/rpc/v0",
-    // url: "http://localhost:1234/rpc/v0",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      jsonrpc: "2.0",
-      method: method,
-      params: params,
-      id: 1,
-    }),
-  };
-  const res = await request(options);
-  return JSON.parse(res.body).result;
+    var options = {
+        method: "POST",
+        url: "https://wallaby.node.glif.io/rpc/v0",
+        // url: "http://localhost:1234/rpc/v0",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            jsonrpc: "2.0",
+            method: method,
+            params: params,
+            id: 1,
+        }),
+    }
+    const res = await request(options)
+    return JSON.parse(res.body).result
 }
 
-const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY);
+const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY)
 
 module.exports = async ({ deployments }) => {
-  const { deploy } = deployments;
+    const { deploy } = deployments
 
+    const priorityFee = await callRpc("eth_maxPriorityFeePerGas")
+    const f4Address = fa.newDelegatedEthAddress(deployer.address).toString()
+    const nonce = await callRpc("Filecoin.MpoolGetNonce", [f4Address])
 
-  const priorityFee = await callRpc("eth_maxPriorityFeePerGas");
-  const f4Address = fa.newDelegatedEthAddress(deployer.address).toString();
-  const nonce = await callRpc("Filecoin.MpoolGetNonce", [f4Address]);
+    console.log("Wallet Ethereum Address:", deployer.address)
+    console.log("Wallet f4Address: ", f4Address)
+    const chainId = network.config.chainId
+    const tokenToBeMinted = networkConfig[chainId]["tokenToBeMinted"]
 
-  console.log("Wallet Ethereum Address:", deployer.address);
-  console.log("Wallet f4Address: ", f4Address)
+    await deploy("SimpleCoin", {
+        from: deployer.address,
+        args: [tokenToBeMinted],
 
+        // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+        // a large gasLimit. This should be addressed in the following releases.
+        // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
+        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
+        maxPriorityFeePerGas: priorityFee,
+        log: true,
+    })
 
-  await deploy("SimpleCoin", {
-    from: deployer.address,
-    args: [],
-    // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
-    // a large gasLimit. This should be addressed in the following releases.
-    // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
-    // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-    maxPriorityFeePerGas: priorityFee,
-    log: true,
-  });
+    await deploy("MinerAPI", {
+        from: deployer.address,
+        args: [0x0000001],
+        // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+        // a large gasLimit. This should be addressed in the following releases.
+        // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
+        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
+        maxPriorityFeePerGas: priorityFee,
+        log: true,
+    })
 
-  await deploy("MinerAPI", {
-    from: deployer.address,
-    args: [0x0000001],
-    // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
-    // a large gasLimit. This should be addressed in the following releases.
-    // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
-    // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-    maxPriorityFeePerGas: priorityFee,
-    log: true,
-  });
+    await deploy("MarketAPI", {
+        from: deployer.address,
+        args: [],
+        // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+        // a large gasLimit. This should be addressed in the following releases.
+        // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
+        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
+        maxPriorityFeePerGas: priorityFee,
+        log: true,
+    })
+}
 
-  await deploy("MarketAPI", {
-    from: deployer.address,
-    args: [],
-    // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
-    // a large gasLimit. This should be addressed in the following releases.
-    // since Ethereum's legacy transaction format is not supported on FVM, we need to specify
-    // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-    maxPriorityFeePerGas: priorityFee,
-    log: true,
-  });
-};
-
-
-module.exports.tags = ["SimpleCoin", "MinerAPI", "MarketAPI"];
+module.exports.tags = ["SimpleCoin", "MinerAPI", "MarketAPI"]
