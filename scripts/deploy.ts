44e8917811f23d541a529eec6f2efab24ec49185
Alex T <alex@tzapu.com>
2022-12-15 12:26:34 +0200

    updated for latest changes

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,13 +5,16 @@ const util = require("util");
 const request = util.promisify(require("request"));
 
 async function main () {
+    const [deployer] = await ethers.getSigners();
+    console.log("deployer", deployer.address)
+
     let ctr;
     const feeData = await ethers.provider.getFeeData();
-    const nonce = await ethers.provider.getTransactionCount('0x...');
+    const nonce = await ethers.provider.getTransactionCount(deployer.address);
     console.log('before', nonce)
 
     const ctrFactory = await ethers.getContractFactory('SimpleCoin')
-    ctr = (await ctrFactory.deploy({
+    ctr = (await ctrFactory.deploy(1, {
       maxPriorityFeePerGas: feeData.maxPriorityFeePerGas?.mul(2), 
       maxFeePerGas: feeData. maxFeePerGas?.mul(2), 
       type: 2,
@@ -19,7 +22,7 @@ async function main () {
     }))
 
     await ctr.deployTransaction.wait(1);
-    const nonce2 = await ethers.provider.getTransactionCount('0x...');
+    const nonce2 = await ethers.provider.getTransactionCount(deployer.address);
     console.log('after', ctr.address, nonce2, ctr.deployTransaction)
 }
 
