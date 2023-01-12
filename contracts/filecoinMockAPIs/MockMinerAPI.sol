f48d76dc57bbfa70c9c6e1fa47bb50ff839cbd55
Zak Ayesh <zakayesh@gmail.com>
2023-01-12 11:31:16 -0500

    updated mock names

--- contracts/filecoinMockAPIs/MockMinerAPI.sol
+++ contracts/filecoinMockAPIs/MockMinerAPI.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.4.25 <=0.8.17;
 
-import "./types/MinerTypes.sol";
+import "./types/MockMinerTypes.sol";
 
 /// @title This contract is a proxy to a built-in Miner actor. Calling one of its methods will result in a cross-actor call being performed. However, in this mock library, no actual call is performed.
 /// @author Zondax AG
 /// @dev Methods prefixed with mock_ will not be available in the real library. These methods are merely used to set mock state. Note that this interface will likely break in the future as we align it
 //       with that of the real library!
-contract MinerAPI {
+contract MockMinerAPI {
     string owner;
     bool isBeneficiarySet = false;
     CommonTypes.ActiveBeneficiary activeBeneficiary;
