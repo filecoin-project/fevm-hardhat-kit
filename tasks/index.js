bda65d49686463fce39af68af91184893b00890e
Zak Ayesh <zakayesh@gmail.com>
2023-01-13 10:54:01 -0500

    added deploy script and tasts for FilecoinMarketConsumer

--- tasks/index.js
+++ tasks/index.js
@@ -1,3 +1,4 @@
 exports.getBalance = require("./simple-coin/get-balance")
 exports.getAddress = require("./get-address")
 exports.sendCoin = require("./simple-coin/send-coin")
+exports.storeAll = require("./filecoin-market-consumer/store-all")
