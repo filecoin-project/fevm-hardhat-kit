44db0cc2f506ca97a5c4f071051a268314335dc4
Zak Ayesh <zakayesh@gmail.com>
2023-01-26 16:02:23 -0500

    update deploy script

--- deploy/00_deploy.js
+++ deploy/00_deploy.js
@@ -1,78 +1,35 @@
 require("hardhat-deploy")
 require("hardhat-deploy-ethers")
 
-const ethers = require("ethers")
-const util = require("util")
-const request = util.promisify(require("request"))
 const { networkConfig } = require("../helper-hardhat-config")
 
-const DEPLOYER_PRIVATE_KEY = network.config.accounts[0]
 
-async function callRpc(method, params) {
-    var options = {
-        method: "POST",
-        url: "https://api.hyperspace.node.glif.io/rpc/v1",
-        // url: "http://localhost:1234/rpc/v0",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            jsonrpc: "2.0",
-            method: method,
-            params: params,
-            id: 1,
-        }),
-    }
-    const res = await request(options)
-    return JSON.parse(res.body).result
-}
-
-const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY)
+const private_key = network.config.accounts[0]
+const wallet = new ethers.Wallet(private_key, ethers.provider)
 
 module.exports = async ({ deployments }) => {
-    const { deploy } = deployments
-
-    const priorityFee = await callRpc("eth_maxPriorityFeePerGas")
-    
-    // Wraps Hardhat's deploy, logging errors to console.
-    const deployLogError = async (title, obj) => {
-        let ret;
-        try {
-            ret = await deploy(title, obj);
-        } catch (error) {
-            console.log(error.toString())
-            process.exit(1)
-        }
-        return ret;
-    }
-
-    console.log("Wallet Ethereum Address:", deployer.address)
+    console.log("Wallet Ethereum Address:", wallet.address)
     const chainId = network.config.chainId
-    const tokenToBeMinted = networkConfig[chainId]["tokenToBeMinted"]
+    const tokensToBeMinted = networkConfig[chainId]["tokensToBeMinted"]
 
+    //deploy Simplecoin
+    const SimpleCoin = await ethers.getContractFactory('SimpleCoin', wallet);
+    console.log('Deploying Simplecoin...');
+    const simpleCoin = await SimpleCoin.deploy(tokensToBeMinted);
+    await simpleCoin.deployed()
+    console.log('SimpleCoin deployed to:', simpleCoin.address);
 
-    await deployLogError("SimpleCoin", {
-        from: deployer.address,
-        args: [tokenToBeMinted],
-        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-        maxPriorityFeePerGas: priorityFee,
-        log: true,
-    })
+    //deploy FilecoinMarketConsumer
+    const FilecoinMarketConsumer = await ethers.getContractFactory('FilecoinMarketConsumer', wallet);
+    console.log('Deploying FilecoinMarketConsumer...');
+    const filecoinMarketConsumer = await FilecoinMarketConsumer.deploy();
+    await filecoinMarketConsumer.deployed()
+    console.log('FilecoinMarketConsumer deployed to:', filecoinMarketConsumer.address);
 
-    await deployLogError("FilecoinMarketConsumer", {
-        from: deployer.address,
-        args: [],
-        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-        maxPriorityFeePerGas: priorityFee,
-        log: true,
-    })
-
-    await deployLogError("DealRewarder", {
-        from: deployer.address,
-        args: [],
-        // maxPriorityFeePerGas to instruct hardhat to use EIP-1559 tx format
-        maxPriorityFeePerGas: priorityFee,
-        log: true,
-    })
+    //deploy DealRewarder
+    const DealRewarder = await ethers.getContractFactory('DealRewarder', wallet);
+    console.log('Deploying DealRewarder...');
+    const dealRewarder = await DealRewarder.deploy();
+    await dealRewarder.deployed()
+    console.log('DealRewarder deployed to:', dealRewarder.address);
 }
-
