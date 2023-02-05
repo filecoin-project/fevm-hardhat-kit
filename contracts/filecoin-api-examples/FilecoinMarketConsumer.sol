215044c9a4ae4f3cb42c25dc7c8b1e874bf69b00
Web3HackerDAO <web3hackerdao@gmail.com>
2023-02-05 23:29:33 +0800

    🐛 Fix bug for FilecoinMarketConsumer type error

--- contracts/filecoin-api-examples/FilecoinMarketConsumer.sol
+++ contracts/filecoin-api-examples/FilecoinMarketConsumer.sol
@@ -11,7 +11,7 @@ contract FilecoinMarketConsumer {
     MarketTypes.GetDealDataCommitmentReturn public dealCommitment;
     MarketTypes.GetDealTermReturn public dealTerm;
     MarketTypes.GetDealEpochPriceReturn public dealPricePerEpoch;
-    MarketTypes.GetDealClientCollateralReturn public clientCollateral; 
+    MarketTypes.GetDealClientCollateralReturn public clientCollateral;
     MarketTypes.GetDealProviderCollateralReturn public providerCollateral;
     MarketTypes.GetDealActivationReturn public activationStatus;
 
@@ -26,45 +26,57 @@ contract FilecoinMarketConsumer {
         storeProviderCollateral(dealId);
         storeDealVerificaton(dealId);
         storeDealActivationStatus(dealId);
-    } 
+    }
 
-    function storeDealLabel(uint64 dealId) public  {
-        dealLabel = MarketAPI.getDealLabel(dealId).label;
+    function storeDealLabel(uint64 dealId) public {
+        dealLabel = MarketAPI.getDealLabel(MarketTypes.GetDealLabelParams(dealId)).label;
     }
 
     function storeDealClient(uint64 dealId) public {
-        dealClientActorId = MarketAPI.getDealClient(dealId).client;
+        dealClientActorId = MarketAPI.getDealClient(MarketTypes.GetDealClientParams(dealId)).client;
     }
 
     function storeDealClientProvider(uint64 dealId) public {
-        dealProviderActorId = MarketAPI.getDealProvider(dealId).provider;
+        dealProviderActorId = MarketAPI
+            .getDealProvider(MarketTypes.GetDealProviderParams(dealId))
+            .provider;
     }
 
     function storeDealCommitment(uint64 dealId) public {
-        dealCommitment = MarketAPI.getDealDataCommitment(dealId);
+        dealCommitment = MarketAPI.getDealDataCommitment(
+            MarketTypes.GetDealDataCommitmentParams(dealId)
+        );
     }
 
     function storeDealTerm(uint64 dealId) public {
-        dealTerm = MarketAPI.getDealTerm(dealId);
+        dealTerm = MarketAPI.getDealTerm(MarketTypes.GetDealTermParams(dealId));
     }
 
     function storeDealTotalPrice(uint64 dealId) public {
-       dealPricePerEpoch = MarketAPI.getDealTotalPrice(dealId);
+        dealPricePerEpoch = MarketAPI.getDealTotalPrice(
+            MarketTypes.GetDealEpochPriceParams(dealId)
+        );
     }
 
     function storeClientCollateral(uint64 dealId) public {
-        clientCollateral = MarketAPI.getDealClientCollateral(dealId);
+        clientCollateral = MarketAPI.getDealClientCollateral(
+            MarketTypes.GetDealClientCollateralParams(dealId)
+        );
     }
-    
+
     function storeProviderCollateral(uint64 dealId) public {
-        providerCollateral = MarketAPI.getDealProviderCollateral(dealId);
+        providerCollateral = MarketAPI.getDealProviderCollateral(
+            MarketTypes.GetDealProviderCollateralParams(dealId)
+        );
     }
 
     function storeDealVerificaton(uint64 dealId) public {
-        isDealActivated = MarketAPI.getDealVerified(dealId).verified;
+        isDealActivated = MarketAPI
+            .getDealVerified(MarketTypes.GetDealVerifiedParams(dealId))
+            .verified;
     }
 
     function storeDealActivationStatus(uint64 dealId) public {
-        activationStatus = MarketAPI.getDealActivation(dealId);
+        activationStatus = MarketAPI.getDealActivation(MarketTypes.GetDealActivationParams(dealId));
     }
 }
