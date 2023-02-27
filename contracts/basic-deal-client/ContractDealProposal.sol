d1adcfda72446013f09161faaffef4cf55351eda
Anton Evangelatov <anton.evangelatov@gmail.com>
2023-02-27 22:58:02 +0100

    wip

---
+++ contracts/basic-deal-client/ContractDealProposal.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@zondax/filecoin-solidity/contracts/v0.8/cbor/BigIntCbor.sol";
+//import "@zondax/filecoin-solidity/contracts/v0.8/external/CBOR.sol";
+import "@zondax/filecoin-solidity/contracts/v0.8/utils/CborDecode.sol";
+import "@zondax/filecoin-solidity/contracts/v0.8/cbor/FilecoinCbor.sol";
+import { CommonTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/CommonTypes.sol";
+
+
+using CBOR for CBOR.CBORBuffer;
+using CBORDecoder for bytes;
+using BigIntCBOR for CommonTypes.BigInt;
+using BigIntCBOR for bytes;
+using FilecoinCBOR for CBOR.CBORBuffer;
+
+struct ContractDealProposal {
+    bytes piece_cid;
+    uint64 piece_size;
+    bool verified_deal;
+    bytes client;
+    bytes provider;
+    string label;
+    int64 start_epoch;
+    int64 end_epoch;
+    CommonTypes.BigInt storage_price_per_epoch;
+    CommonTypes.BigInt provider_collateral;
+    CommonTypes.BigInt client_collateral;
+
+    string version;
+    bytes params;
+}
+
+
+function serializeContractDealProposal(ContractDealProposal memory dealProposal) pure returns (bytes memory) {
+    // FIXME what should the max length be on the buffer?
+    CBOR.CBORBuffer memory buf = CBOR.create(64);
+
+    buf.startFixedArray(13);
+
+    buf.writeCid(dealProposal.piece_cid);
+    buf.writeUInt64(dealProposal.piece_size);
+    buf.writeBool(dealProposal.verified_deal);
+    buf.writeBytes(dealProposal.client);
+    buf.writeBytes(dealProposal.provider);
+    buf.writeString(dealProposal.label);
+    buf.writeInt64(dealProposal.start_epoch);
+    buf.writeInt64(dealProposal.end_epoch);
+    buf.writeBytes(dealProposal.storage_price_per_epoch.serializeBigInt());
+    buf.writeBytes(dealProposal.provider_collateral.serializeBigInt());
+    buf.writeBytes(dealProposal.client_collateral.serializeBigInt());
+    buf.writeString(dealProposal.version);
+    buf.writeBytes(dealProposal.params);
+
+    return buf.data();
+}
+
+
+function deserializeContractDealProposal(bytes memory rawResp) pure returns (ContractDealProposal memory ret) {
+    uint byteIdx = 0;
+    uint len;
+
+    (len, byteIdx) = rawResp.readFixedArray(byteIdx);
+    assert(len == 13);
+
+    (ret.piece_cid, byteIdx) = rawResp.readBytes(byteIdx);
+    (ret.piece_size, byteIdx) = rawResp.readUInt64(byteIdx);
+    (ret.verified_deal, byteIdx) = rawResp.readBool(byteIdx);
+    (ret.client, byteIdx) = rawResp.readBytes(byteIdx);
+    (ret.provider, byteIdx) = rawResp.readBytes(byteIdx);
+
+    (ret.label, byteIdx) = rawResp.readString(byteIdx);
+    (ret.start_epoch, byteIdx) = rawResp.readInt64(byteIdx);
+    (ret.end_epoch, byteIdx) = rawResp.readInt64(byteIdx);
+
+    bytes memory storage_price_per_epoch_bytes;
+    (storage_price_per_epoch_bytes, byteIdx) = rawResp.readBytes(byteIdx);
+    ret.storage_price_per_epoch = storage_price_per_epoch_bytes.deserializeBigInt();
+
+    bytes memory provider_collateral_bytes;
+    (provider_collateral_bytes, byteIdx) = rawResp.readBytes(byteIdx);
+    ret.provider_collateral = provider_collateral_bytes.deserializeBigInt();
+
+    bytes memory client_collateral_bytes;
+    (client_collateral_bytes, byteIdx) = rawResp.readBytes(byteIdx);
+    ret.client_collateral = client_collateral_bytes.deserializeBigInt();
+
+    (ret.version, byteIdx) = rawResp.readString(byteIdx);
+    (ret.params, byteIdx) = rawResp.readBytes(byteIdx);
+
+}
+
+
+
+
--- contracts/basic-deal-client/DealClient.sol
+++ contracts/basic-deal-client/DealClient.sol
@@ -1,78 +1,174 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import {specific_authenticate_message_params_parse, specific_deal_proposal_cbor_parse} from "./CBORParse.sol";
+
+import { MarketAPI } from "@zondax/filecoin-solidity/contracts/v0.8/MarketAPI.sol";
+import { CommonTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/CommonTypes.sol";
+import { MarketTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/MarketTypes.sol";
+import { AccountTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/AccountTypes.sol";
+import { CommonTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/CommonTypes.sol";
+import { AccountCBOR } from "@zondax/filecoin-solidity/contracts/v0.8/cbor/AccountCbor.sol";
+import { MarketCBOR } from "@zondax/filecoin-solidity/contracts/v0.8/cbor/MarketCbor.sol";
+import { BytesCBOR } from "@zondax/filecoin-solidity/contracts/v0.8/cbor/BytesCbor.sol";
+import { BigNumbers, BigNumber } from "@zondax/solidity-bignumber/src/BigNumbers.sol";
+import { ContractDealProposal, serializeContractDealProposal, deserializeContractDealProposal} from "./ContractDealProposal.sol";
+
+import "hardhat/console.sol";
+
+
+contract MockMarket {
+    function publish_deal(bytes memory raw_auth_params, address callee) public {
+        // calls standard filecoin receiver on message authentication api method number
+        (bool success, ) = callee.call(abi.encodeWithSignature("handle_filecoin_method(uint64,uint64,bytes)", 0, 2643134072, raw_auth_params));
+        require(success, "client contract failed to authorize deal publish");
+    }
+}
+
+struct ProposalIdSet {
+    bytes32 proposalId;
+    bool valid;
+}
+
+struct ProviderSet {
+    bytes provider;
+    bool valid;
+}
 
 contract DealClient {
-  uint64 constant public AUTHORIZE_MESSAGE_METHOD_NUM = 2643134072;
-  uint64 constant public DATACAP_RECEIVER_HOOK_METHOD_NUM = 3726118371;
 
+    using AccountCBOR for *;
+    //using BigInt for CommonTypes;
+    using MarketCBOR for *;
 
-  mapping(bytes => bool) public cidSet;
-  mapping(bytes => uint) public cidSizes;
-  mapping(bytes => mapping(bytes => bool)) public cidProviders;
+    uint64 constant public AUTHENTICATE_MESSAGE_METHOD_NUM = 2643134072;
+    uint64 constant public DATACAP_RECEIVER_HOOK_METHOD_NUM = 3726118371;
+    uint64 constant public MARKET_NOTIFY_DEAL_METHOD_NUM = 4186741094;
 
-  mapping(bytes32 => bytes) public proposals;
+    mapping(bytes32 => bytes) public dealProposals; // dealID -> dealProposalBytes
+    mapping(bytes => ProposalIdSet) public pieceToProposal; // commP -> dealProposalID
+    mapping(bytes => ProviderSet) public pieceProviders; // commP -> provider
+    mapping(bytes => uint64) public pieceDeals; // commP -> deal ID
 
-  event ReceivedDataCap(string received);
-  event DealProposalCreate(bytes32 indexed id, bytes size, bool indexed verified, uint price);
+    event ReceivedDataCap(string received);
+    event DealProposalCreate(bytes32 indexed id, uint64 size, bool indexed verified, uint256 price);
 
-  address public owner;
+    address public owner;
 
-  constructor() {
-    owner = msg.sender;
-  }
-
-  function addCID(bytes calldata cidraw, uint size) public {
-    require(msg.sender == owner);
-    cidSet[cidraw] = true;
-    cidSizes[cidraw] = size;
-  }
-
-  function policyOK(bytes calldata cidraw, bytes calldata provider) internal view returns (bool) {
-    bool alreadyStoring = cidProviders[cidraw][provider];
-    return !alreadyStoring;
-  }
-
-  function authorizeData(bytes calldata cidraw, bytes calldata provider, uint size) public {
-    // if (msg.sender != f05) return;
-    require(cidSet[cidraw], "cid must be added before authorizing");
-    require(cidSizes[cidraw] == size, "data size must match expected");
-    require(policyOK(cidraw, provider), "deal failed policy check: has provider already claimed this cid?");
-
-    cidProviders[cidraw][provider] = true;
-  }
-
-  function handle_filecoin_method(uint64 method, uint64, bytes calldata params) public {
-    if (method == AUTHORIZE_MESSAGE_METHOD_NUM) {
-      return;
-      bytes calldata deal_proposal_cbor_bytes = specific_authenticate_message_params_parse(params);
-      (bytes calldata cidraw, bytes calldata provider, uint size) = specific_deal_proposal_cbor_parse(deal_proposal_cbor_bytes);
-      authorizeData(cidraw, provider, size);
-    } else if (method == DATACAP_RECEIVER_HOOK_METHOD_NUM) {
-      emit ReceivedDataCap("DataCap Received!");
-    } else {
-      revert("the filecoin method that was called is not handled");
+    constructor() {
+        owner = msg.sender;
     }
-  }
 
-  function makeDealProposal(bytes memory _deal, bytes memory size, bool verified, uint price) public returns (bytes32) {
-    bytes32 _id = keccak256(abi.encodePacked(block.timestamp, msg.sender));
-    proposals[_id] = _deal;
+    // FOR DEMO PURPOSES
+    function simpleDealProposal(bytes memory pieceCid, uint64 pieceSize) public {
+        ContractDealProposal memory deal;
+        deal.piece_cid = pieceCid;
+        deal.piece_size = pieceSize;
+        deal.storage_price_per_epoch = uintToBigInt(0);
 
-    emit DealProposalCreate(_id, size, verified, price);
+        bytes memory serDeal = serializeContractDealProposal(deal);
+        makeDealProposal(serDeal);
 
-    return _id;
-  }
+    }
 
-  function getDealProposal(bytes32 id) public view returns(bytes memory) {
-    return proposals[id];
-  }
+    function makeDealProposal(bytes memory deal) public {
+        // TODO: evaluate permissioning here
+        require(msg.sender == owner);
 
-  function publish_deal(bytes memory raw_auth_params, address callee) public {
-    // calls standard filecoin receiver on message authentication api method number
-    (bool success, ) = callee.call(abi.encodeWithSignature("handle_filecoin_method(uint64,uint64,bytes)", 0, 2643134072, raw_auth_params));
-    require(success, "client contract failed to authorize deal publish");
-  }
+        // creates a unique ID for the deal proposal -- there are many ways to do this
+        bytes32 _id = keccak256(abi.encodePacked(block.timestamp, msg.sender));
+        dealProposals[_id] = deal;
+
+        ContractDealProposal memory proposal = deserializeContractDealProposal(deal);
+        pieceToProposal[proposal.piece_cid] = ProposalIdSet(_id, true);
+
+        // writes the proposal metadata to the event log
+        emit DealProposalCreate(_id, proposal.piece_size, proposal.verified_deal, bigIntToUint(proposal.storage_price_per_epoch));
+    }
+
+
+    function getDealProposal(bytes32 proposalId) view public returns (bytes memory) {
+        return dealProposals[proposalId];
+    }
+
+
+    function authenticateMessage(bytes memory params) view internal {
+
+        AccountTypes.AuthenticateMessageParams memory amp = params.deserializeAuthenticateMessageParams();
+        //TODO: fix DealProposal deserialization
+        //MarketTypes.DealProposal memory proposal = amp.message.deserializeDealProposal();
+
+        //require(pieceToProposal[proposal.piece_cid].valid, "piece cid must be added before authorizing");
+        //require(!pieceProviders[proposal.piece_cid].valid, "deal failed policy check: provider already claimed this cid");
+    }
+
+    function dealNotify(bytes memory params) internal {
+
+        MarketTypes.MarketDealNotifyParams memory mdnp = params.deserializeMarketDealNotifyParams();
+        MarketTypes.DealProposal memory proposal = mdnp.dealProposal.deserializeDealProposal();
+
+        require(pieceToProposal[proposal.piece_cid].valid, "piece cid must be added before authorizing");
+        require(!pieceProviders[proposal.piece_cid].valid, "deal failed policy check: provider already claimed this cid");
+
+        pieceProviders[proposal.piece_cid] = ProviderSet(proposal.provider, true);
+        pieceDeals[proposal.piece_cid] = mdnp.dealId;
+
+    }
+
+    // client - filecoin address byte format
+    function addBalance(bytes memory client, uint256 value) public {
+
+        require(msg.sender == owner);
+
+        // TODO:: remove first arg
+        // change to ethAddr -> actorId and use that in the below API
+
+        MarketAPI.addBalance(client, value);
+    }
+
+    // Below 2 funcs need to go to filecoin.sol
+    function uintToBigInt(uint256 value) internal view returns(CommonTypes.BigInt memory) {
+        BigNumber memory bigNumVal = BigNumbers.init(value, false);
+        CommonTypes.BigInt memory bigIntVal = CommonTypes.BigInt(bigNumVal.val, bigNumVal.neg);
+        return bigIntVal;
+    }
+
+    function bigIntToUint(CommonTypes.BigInt memory bigInt) internal view returns (uint256) {
+        BigNumber memory bigNumUint = BigNumbers.init(bigInt.val, bigInt.neg);
+        uint256 bigNumExtractedUint = uint256(bytes32(bigNumUint.val));
+        return bigNumExtractedUint;
+    }
+
+
+    function withdrawBalance(bytes memory client, uint256 value) public returns(uint) {
+        // TODO:: remove first arg
+        // change to ethAddr -> actorId and use that in the below API
+
+        require(msg.sender == owner);
+
+        MarketTypes.WithdrawBalanceParams memory params = MarketTypes.WithdrawBalanceParams(client, uintToBigInt(value));
+        MarketTypes.WithdrawBalanceReturn memory ret = MarketAPI.withdrawBalance(params);
+
+        return bigIntToUint(ret.amount_withdrawn);
+    }
+
+    function receiveDataCap(bytes memory params) internal {
+        emit ReceivedDataCap("DataCap Received!");
+        // Add get datacap balance api and store datacap amount
+    }
+
+
+    function handle_filecoin_method(uint64 method, uint64, bytes memory params) public {
+        // dispatch methods
+        if (method == AUTHENTICATE_MESSAGE_METHOD_NUM) {
+            authenticateMessage(params);
+        } else if (method == MARKET_NOTIFY_DEAL_METHOD_NUM) {
+            dealNotify(params);
+        }
+        else if (method == DATACAP_RECEIVER_HOOK_METHOD_NUM) {
+            receiveDataCap(params);
+        } else {
+            revert("the filecoin method that was called is not handled");
+        }
+    }
 }
 
