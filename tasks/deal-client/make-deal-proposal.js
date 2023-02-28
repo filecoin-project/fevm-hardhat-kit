c8ea089ce088e9e7c366755850b068e78aab7526
Zak Ayesh <zakayesh@gmail.com>
2023-02-28 15:54:07 -0700

    fix camelCase issue

--- tasks/deal-client/make-deal-proposal.js
+++ tasks/deal-client/make-deal-proposal.js
@@ -5,40 +5,38 @@ task(
     "Makes a deal proposal via the client contract. This will ultimately emit an event that storage providers can listen too and choose to accept your deal."
   )
     .addParam("contract", "The address of the deal client solidity")
-    .addParam("piececid", "The address of the DealRewarder contract")
-    .addParam("piecesize", "The piece CID of the data you want to put up a bounty for")
-    .addParam("verifieddeal", "Size of the data you are putting a bounty on")
-    .addParam("clientaddr", "Address of client")
+    .addParam("pieceCid", "The address of the DealRewarder contract")
+    .addParam("pieceSize", "The piece CID of the data you want to put up a bounty for")
+    .addParam("verifiedDeal", "Size of the data you are putting a bounty on")
     .addParam("label", "The deal label (typically the raw cid)")
-    .addParam("startepoch", "The epoch the deal will start")
-    .addParam("endepoch", "The epoch the deal will end")
-    .addParam("storage-price-per-epoch", "The cost of the deal, in FIL, per epoch")
-    .addParam("providercollateral", "The collateral, in FIL, to be put up by the storage provider")
-    .addParam("clientcollateral", "The collateral, in FIL, to be put up by the the client (which is you)")
-    .addParam("extraparams-version", "")
-    .addParam("locationref", "Where the data you want to be stored is located")
-    .addParam("carsize", "The size of the .car file")
-    .addParam("skip-ipni-announce", "")
-    .addParam("remove-unsealed-copy", "")
+    .addParam("startEpoch", "The epoch the deal will start")
+    .addParam("endEpoch", "The epoch the deal will end")
+    .addParam("storagePricePerEpoch", "The cost of the deal, in FIL, per epoch")
+    .addParam("providerCollateral", "The collateral, in FIL, to be put up by the storage provider")
+    .addParam("clientCollateral", "The collateral, in FIL, to be put up by the the client (which is you)")
+    .addParam("extraParamsVersion", "")
+    .addParam("locationRef", "Where the data you want to be stored is located")
+    .addParam("carSize", "The size of the .car file")
+    .addParam("skipIpniAnnounce", "")
+    .addParam("removeUnsealedCopy", "")
     .setAction(async (taskArgs) => {
         //store taskargs as useable variables
         const contractAddr = taskArgs.contract
-        const cid = taskArgs.piececid
-        const pieceSize = taskArgs.piecesize
+        const cid = taskArgs.pieceCid
+        const pieceSize = taskArgs.pieceSize
         const verifiedDeal = taskArgs.verifiedDeal
-        const clientAddr = taskArgs.clientaddr
         const label = taskArgs.label
-        const startepoch = taskArgs.startepoch
-        const endepoch = taskArgs.endepoch
-        const pricePerEpoch = taskArgs.storage-price-per-epoch
+        const startepoch = taskArgs.startEpoch
+        const endepoch = taskArgs.endEpoch
+        const pricePerEpoch = taskArgs.storagePricePerEpoch
         const providerCollateral = taskArgs.providerCollateral
         const clientCollateral = taskArgs.clientCollateral 
-        const extraParamsVersion = taskArgs.extraparams-version
+        const extraParamsVersion = taskArgs.extraParamsVersion
         const extraParams = {
-            locationRef: taskArgs.locationref,
-            carSize: taskArgs.carsize,
-            skipIpniAnnounce: taskArgs.skip-ipni-announce,
-            removeUnsealedCopy: taskArgs.remove-unsealed-copy
+            locationRef: taskArgs.locationRef,
+            carSize: taskArgs.carSize,
+            skipIpniAnnounce: taskArgs.skipIpniAnnounce,
+            removeUnsealedCopy: taskArgs.removeUnsealedCopy
         }
         const networkId = network.name
         console.log("Making deal proposal on network", networkId)
@@ -61,7 +59,6 @@ task(
             cidHex, 
             pieceSize,
             verifiedDeal,
-            clientAddr,
             label,
             startepoch,
             endepoch,
