b98e845b2ef26bf076f990576a0849492f6a3819
Matt Hamilton <matt@dharach.com>
2023-04-17 17:00:52 +0200

    Add support for Localnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,6 +19,11 @@ module.exports = {
       },
     defaultNetwork: "Hyperspace",
     networks: {
+        Localnet: {
+            chainId: 31415926,
+            url: "http://127.0.0.1:1234/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
         Hyperspace: {
             chainId: 3141,
             url: "https://api.hyperspace.node.glif.io/rpc/v1",
