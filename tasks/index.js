4c4e3ba7aeaea444cb3b113bbd466520026e337a
Aayush Gupta <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2023-04-19 15:59:26 +0530

    Added cid-to-bytes in index

--- tasks/index.js
+++ tasks/index.js
@@ -1,5 +1,6 @@
 exports.getBalance = require("./simple-coin/get-balance")
 exports.getAddress = require("./get-address")
+exports.getAddress = require("./cid-to-bytes")
 exports.sendCoin = require("./simple-coin/send-coin")
 exports.storeAll = require("./filecoin-market-consumer/store-all")
 exports.addCID = require("./deal-rewarder/add-cid")
