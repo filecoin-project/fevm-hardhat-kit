f3a35d1062638088fd51cd16c7a25b36013acd5a
Aayush Gupta <guptaaayush432@gmail.com>
2023-04-19 17:20:49 +0530

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,13 @@ module.exports = {
     solidity: {
         version: "0.8.17",
         settings: {
-          optimizer: {
-            enabled: true,
-            runs: 1000,
-            details: { yul: false },
-          },
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+                details: { yul: false },
+            },
         },
-      },
+    },
     defaultNetwork: "Hyperspace",
     networks: {
         Localnet: {
