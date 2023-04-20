e342b0c074dd5175bb263803439b5f2575dc0cd5
Aayush Gupta <guptaaayush432@gmail.com>
2023-04-20 15:44:36 +0530

    added CID TO HEX logic

--- tasks/deal-client/get-deal-id.js
+++ tasks/deal-client/get-deal-id.js
@@ -1,16 +1,22 @@
+const CID = require("cids")
+
 task("get-deal-id", "Gets a deal proposal from the proposal id")
     .addParam("contract", "The address of the deal client solidity")
     .addParam("cid", "The proposal ID")
     .setAction(async (taskArgs) => {
         const contractAddr = taskArgs.contract
-        console.log("working")
         const cid = taskArgs.cid
-        console.log("working")
         const networkId = network.name
         console.log("Getting deal proposal on network", networkId)
 
         //create a new wallet instance
         const wallet = new ethers.Wallet(network.config.accounts[0], ethers.provider)
+
+        //convert piece CID string to hex bytes
+        const cidHexRaw = new CID(cid).toString("base16").substring(1)
+        const cidHex = "0x00" + cidHexRaw
+        console.log("Hex bytes are:", cidHex)
+        console.log("Complete!")
 
         //create a DealClient contract factory
         const DealClient = await ethers.getContractFactory("DealClient", wallet)
@@ -23,27 +29,19 @@ task("get-deal-id", "Gets a deal proposal from the proposal id")
         let result = await dealClient.getDealId(Cid)
         console.log("The deal ID is:", result)
 
-        let result1 = await dealClient.getDealId(
-            "0x0001701220a08ffd458e5cc5a24281b27e5cce6fea7112d9364120bc6e8cf36ee8233cc417"
-        )
+        let result1 = await dealClient.getDealId(cidHex)
         console.log("The deal ID is:", result1)
 
         // getPieceStatus
-        let getPieceStatus = await dealClient.getPieceStatus(
-            "0x0001701220a08ffd458e5cc5a24281b27e5cce6fea7112d9364120bc6e8cf36ee8233cc417"
-        )
+        let getPieceStatus = await dealClient.getPieceStatus(cidHex)
         console.log("The getPieceStatus is:", getPieceStatus)
 
         // getProviderSet
-        let getProviderSet = await dealClient.getProviderSet(
-            "0x0001701220a08ffd458e5cc5a24281b27e5cce6fea7112d9364120bc6e8cf36ee8233cc417"
-        )
+        let getProviderSet = await dealClient.getProviderSet(cidHex)
         console.log("The ProviderSet is:", getProviderSet)
 
         // getProposalIdSet
-        let getProposalIdSet = await dealClient.getProviderSet(
-            "0x0001701220a08ffd458e5cc5a24281b27e5cce6fea7112d9364120bc6e8cf36ee8233cc417"
-        )
+        let getProposalIdSet = await dealClient.getProviderSet(cidHex)
         console.log("The ProposalIdSet is:", getProposalIdSet)
 
         // dealsLength
@@ -51,11 +49,8 @@ task("get-deal-id", "Gets a deal proposal from the proposal id")
         console.log("The dealsLength is:", dealsLength)
 
         // getDealByIndex
-        let getDealByIndex = await dealClient.getDealByIndex(0)
+        let getDealByIndex = await dealClient.getDealByIndex(1)
         console.log("The DealByIndex is:", getDealByIndex)
-
-        // let result1 = await dealClient.getDealId(cid)
-        // console.log("The deal ID is:", result1)
     })
 
 // new TextEncoder("utf-8").encode("BE_0001"),
